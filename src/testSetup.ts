1ca116c3d3c05445f26c2e15fa26a21d92908a8a
João Marcelo Souza <joaomarcelo9393+github@gmail.com>
2021-12-01 20:35:18 -0300

    Transpile TS with Babel instead of Webpack
    The motivation is to have a single TS loader for all environments.
    
    Previously, the dev environment would rely on Webpack's ts-loader
    while Jest would rely on @babel/preset-typescript in .babelrc test env.
    
    Now, all environments rely on @babel/preset-typescript in .babelrc.
    Gotcha: that preset doesn't perform type checking, only transpiling.
    To do type checking, I introduced ForkTsCheckerWebpackPlugin.
    
    As a side effect of this change, the application crashed with an error:
    `regeneratorRuntime is not defined`. I'm not totally sure, but I believe
    it's because Webpack handles async/await by default while Babel doesn't.
    
    To fix that, I introduced @babel/plugin-transform-runtime in .babelrc.
    Since Jest uses .babelrc, it's no longer necessary to import
    `regenerator-runtime/runtime` in test files, so I removed it.

--- src/testSetup.ts
+++ src/testSetup.ts
@@ -1,10 +1,6 @@
 // This file gets loaded before every ".test" file.
 // See the "jest" key in package.json
 
-// Enable using async/await in test files.
-// https://stackoverflow.com/a/57439821
-import 'regenerator-runtime/runtime'
-
 // Provides custom DOM element matchers for Jest.
 // https://testing-library.com/docs/ecosystem-jest-dom/
 import '@testing-library/jest-dom'
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const Dotenv = require("dotenv-webpack")
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -9,41 +10,43 @@ module.exports = {
   module: {
     rules: [
       {
-        // Enable JSX (find options in .babelrc)
-        test: /\.(js|jsx)$/,
+        // Load Babel-transpiled files (see .babelrc)
+        test: /\.(js|jsx|ts|tsx)$/,
         exclude: /(node_modules)/,
         loader: "babel-loader",
       },
       {
-        // Enable TypeScript
-        test: /\.(ts|tsx)?$/,
-        exclude: /node_modules/,
-        loader: "ts-loader",
-      },
-      {
-        // Enable importing CSS from React components
+        // Load CSS files
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
       },
       {
-        // Enable importing SVG as a React component
+        // Load SVGs as React components
         test: /\.svg$/,
         loader: "@svgr/webpack"
       },
     ]
   },
-  resolve: { extensions: ["*", ".js", ".jsx", ".ts", ".tsx"] },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
+  },
+  // Output the bundle as ./dist/bundle.js
   output: {
     path: path.resolve(__dirname, "dist/"),
     publicPath: "/dist/",
     filename: "bundle.js"
   },
   devServer: {
-    historyApiFallback: true, // let React handle routes
+    // Let React handle routes
+    historyApiFallback: true,
     port: 3000
   },
   plugins: [
+    // Enable hot refresh for React (see .babelrc)
     new ReactRefreshWebpackPlugin(),
+    // Load environment variables from a .env file at the root folder
     new Dotenv(),
+    // Perform TypeScript type check on a separate thread
+    new ForkTsCheckerWebpackPlugin(),
   ]
 };
